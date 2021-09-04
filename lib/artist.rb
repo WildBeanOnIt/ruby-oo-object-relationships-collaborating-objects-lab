e4fdd7410129081d98a4909b7afec9f0247d0c32
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-09-03 21:25:28 -0500

    commit tree 5715385d37c5d620d3d6ce8a6d89ffdd92198df4

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class Artist
+
+    @@all = []
+
+    attr_accessor :name
+
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    
+
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @song = []
+    end
+
+    def songs
+        @song << song
+    end
+
+
+
+
+end
